f3aaeafc15cd5c0dcb2657ee9aec8cf2e6b10d13
mariliafernandes <f.mariliafernandes@gmail.com>
2022-04-14 18:19:58 -0300

    inicializaçao do projeto

--- teste.py
+++ teste.py
@@ -1,44 +1,12 @@
-class Conta:
+def cria_conta(numero, titular, saldo, limite):
+    conta = {"numero": numero, "titular": titular, "saldo": saldo, "limite": limite}
+    return conta
 
-    def __init__(self, numero, titular, saldo, limite):
-        print(f"Construindo objeto...{self}")
-        self.__numero = numero
-        self.__titular = titular
-        self.__saldo = saldo
-        self.__limite = limite
+def deposita(conta, valor):
+    conta["saldo"] += valor
 
-    def extrato(self):
-        print(f"Saldo de R$ {self.__saldo} do titular {self.__titular}")
+def saca(conta, valor):
+    conta["saldo"] -= valor
 
-    def deposita(self, valor):
-        self.__saldo += valor
-
-    def __pode_sacar(self,valor_a_sacar):
-        valor_disponivel_a_sacar = self.__saldo + self.__limite
-        return valor_a_sacar <= valor_disponivel_a_sacar
-
-    def saca(self, valor):
-        if(self.__pode_sacar(valor)):
-            self.__saldo -= valor
-        else:
-            print(f"O valor {valor} passou do limite")
-
-    def transfere(self,valor,destino):
-        self.saca(valor)
-        destino.deposita(valor)
-
-    @property
-    def saldo(self):
-        return self.__saldo
-
-    @property
-    def titular(self):
-        return self.__titular
-
-    @property
-    def limite(self):
-        return self.__limite
-
-    @limite.setter
-    def limite(self,limite):
-        self.__limite = limite
+def extrato(conta):
+    print(f"Saldo é {conta["saldo"]}")
